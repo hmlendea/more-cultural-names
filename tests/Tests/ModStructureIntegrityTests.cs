7cc70ff7dc9f24624db634cf7425c2cec8b95284
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-10-15 15:47:37 +0300

    Mod structure integrity tests

---
+++ tests/Tests/ModStructureIntegrityTests.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using McnTests.Entities;
+using McnTests.Helpers;
+
+namespace McnTests.Tests
+{
+    [TestClass]
+    public class ModStructureIntegrityTests
+    {
+        const string cultureFileNamePattern = @"zzz_.*\.txt";
+        const string dynastyFileNamePattern = @"99_.*_dynasties\.txt";
+        const string landedTitlesFileNamePattern = @"zzz_.*\.txt";
+        const string localisationFileNamePattern = @"0_.*\.csv";
+
+        [TestMethod]
+        public void TestModDirectories()
+        {
+            Assert.IsTrue(Directory.Exists(ApplicationPaths.CommonDirectory));
+            Assert.IsTrue(Directory.Exists(ApplicationPaths.CulturesDirectory));
+            Assert.IsTrue(Directory.Exists(ApplicationPaths.DynastiesDirectory));
+            Assert.IsTrue(Directory.Exists(ApplicationPaths.LandedTitlesDirectory));
+            Assert.IsTrue(Directory.Exists(ApplicationPaths.LocalisationDirectory));
+        }
+
+        [TestMethod]
+        public void TestModDescriptor()
+        {
+            Assert.IsTrue(File.Exists(ApplicationPaths.DescriptorFile));
+
+            ModDescriptor descriptor = ModDescriptor.FromFile(ApplicationPaths.DescriptorFile);
+
+            string picturePath = Path.Combine(ApplicationPaths.ModDirectory, descriptor.Picture);
+
+            Assert.IsTrue(File.Exists(picturePath));
+            Assert.AreEqual($"mod/{Path.GetFileName(ApplicationPaths.ModDirectory)}", descriptor.Path);
+        }
+
+        [TestMethod]
+        public void TestFileNamesAndExtensions()
+        {
+            List<string> culturesFiles = Directory.GetFiles(ApplicationPaths.CulturesDirectory).ToList();
+            List<string> dynastiesFiles = Directory.GetFiles(ApplicationPaths.DynastiesDirectory).ToList();
+            List<string> landedTitlesFiles = Directory.GetFiles(ApplicationPaths.LandedTitlesDirectory).ToList();
+            List<string> localisationFiles = Directory.GetFiles(ApplicationPaths.LocalisationDirectory).ToList();
+
+            culturesFiles.ForEach(x => Assert.IsTrue(Regex.IsMatch(x, cultureFileNamePattern)));
+            dynastiesFiles.ForEach(x => Assert.IsTrue(Regex.IsMatch(x, dynastyFileNamePattern)));
+            landedTitlesFiles.ForEach(x => Assert.IsTrue(Regex.IsMatch(x, landedTitlesFileNamePattern)));
+            localisationFiles.ForEach(x => Assert.IsTrue(Regex.IsMatch(x, localisationFileNamePattern)));
+        }
+    }
+}
