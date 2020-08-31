c9c9b83138b5da0a20998ba5c7b3134d360975e7
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-01 00:46:56 +0300

    Packaging script

---
+++ scripts/package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+STARTDIR="$(pwd)"
+OUTDIR="$STARTDIR/out"
+VERSION=${1}
+
+if [ -z "${VERSION}" ]; then
+    echo "ERROR: Please specify a version"
+    exit 1
+fi
+
+function build {
+    GAME=${1}
+    INDIR="out/${GAME}/"
+    ZIPNAME=${GAME}_${VERSION}
+
+    echo "Building the '${INDIR}' package..."
+
+    zip -q -r "${ZIPNAME}.zip" "${INDIR}"
+    mv "${ZIPNAME}.zip" "${OUTDIR}/"
+}
+
+build "CK2HIP"
+build "ImperatorRome"
