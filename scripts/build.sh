58ae1692fccbf566392681e83f93b610f9f8351c
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-02 20:23:10 +0300

    Updated mcn-builder to 1.1.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOD_BUILDER_VERSION="1.0.0"
+MOD_BUILDER_VERSION="1.1.0"
 MOD_BUILDER_ZIP_URL="https://github.com/hmlendea/mcn-mod-builder/releases/download/v${MOD_BUILDER_VERSION}/mcn-mod-builder_${MOD_BUILDER_VERSION}_linux-x64.zip"
 MOD_BUILDER_BIN_FILE_PATH="mcn-mod-builder/MoreCulturalNamesModBuilder"
 
