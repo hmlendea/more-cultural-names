1b92d840d5f33b686e81157eccc6dbb5d1675bf5
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-02 21:13:55 +0300

    Updated the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 MOD_BUILDER_VERSION="1.1.0"
-MOD_BUILDER_ZIP_URL="https://github.com/hmlendea/mcn-mod-builder/releases/download/v${MOD_BUILDER_VERSION}/mcn-mod-builder_${MOD_BUILDER_VERSION}_linux-x64.zip"
-MOD_BUILDER_BIN_FILE_PATH="mcn-mod-builder/MoreCulturalNamesModBuilder"
+MOD_BUILDER_NAME="more-cultural-names-builder"
+MOD_BUILDER_ZIP_URL="https://github.com/hmlendea/${MOD_BUILDER_NAME}/releases/download/v${MOD_BUILDER_VERSION}/${MOD_BUILDER_NAME}_${MOD_BUILDER_VERSION}_linux-x64.zip"
+MOD_BUILDER_BIN_FILE_PATH="${MOD_BUILDER_NAME}/MoreCulturalNamesModBuilder"
 
-if [ ! -d "mcn-mod-builder" ]; then
+if [ ! -d ${MOD_BUILDER_NAME} ]; then
     wget -c ${MOD_BUILDER_ZIP_URL}
-    mkdir mcn-mod-builder
-    unzip mcn-mod-builder_${MOD_BUILDER_VERSION}_linux-x64.zip -d mcn-mod-builder
+    mkdir ${MOD_BUILDER_NAME}
+    unzip ${MOD_BUILDER_NAME}_${MOD_BUILDER_VERSION}_linux-x64.zip -d ${MOD_BUILDER_NAME}
 fi
 
 function build {
