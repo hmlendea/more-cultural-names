baa2d54daf651fc59289960d7d1092ce95affdb8
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-04 20:27:00 +0300

    Improved the validation script

--- scripts/find-mistakes.sh
+++ scripts/find-mistakes.sh
@@ -2,3 +2,5 @@
 
 grep -n "parent=\"\([^\"]*\)\"[^>]*>\1<" titles.xml
 grep -n "parent=\"[a-zA-Z][^_]" titles.xml
+grep -n "parent=\"[a-zA-Z][^_]" titles.xml
+grep "GameId game=\"ImperatorRome\"" titles.xml | sed 's/[ \t]*<!--.*-->[ \t]*//g' | sort | uniq -c | sed 's/^[ \t]*//g' | grep "^[2-9]"
