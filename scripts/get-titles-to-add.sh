1c38307beed8fb72816c070fa76c8141dff55f18
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-16 21:18:46 +0300

    Improved script

--- scripts/get-titles-to-add.sh
+++ scripts/get-titles-to-add.sh
@@ -2,14 +2,12 @@
 
 echo "" > titles-to-add.xml
 
-for TITLE_ID in $(grep "^\s*[ekdcb]_[^ \t=]*\s*=" ../ck2-landed-titles-manager/swmh.txt | \
+for TITLE_ID in $(grep "^\s*[ekdcb]_" ../ck2-landed-titles-manager/swmh.txt | \
                     sed 's/\s*=\s*/ = /g' | \
                     sed 's/^\s*//g' | \
                     awk '{print $1}' | \
                     sort | uniq); do
-    if [ -z "$(grep "<GameId game=\"CK2HIP\"" titles.xml | grep ">"${TITLE_ID}"<")" ]; then
-        
-                    
+    if [ -z "$(grep "<GameId game=\"CK2HIP\"[^>]*>${TITLE_ID}" titles.xml)" ]; then
         echo "${TITLE_ID} is not defined"
         echo "  <LocationEntity>" >> titles-to-add.xml
         echo "    <Id>${TITLE_ID:2}</Id>" >> titles-to-add.xml
