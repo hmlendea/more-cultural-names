9f3994e635cd2ceaed419c593566e1a532172e84
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-18 20:28:29 +0300

    Updated the validation script

--- scripts/find-mistakes.sh
+++ scripts/find-mistakes.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-grep -n "parent=\"\([^\"]*\)\"[^>]*>\1<" titles.xml
-grep -n "parent=\"[a-zA-Z][^_]" titles.xml
-grep -n "parent=\"[a-zA-Z][^_]" titles.xml
-
 # Find duplicated IDs
 grep "^ *<Id>" *.xml | \
     sort | uniq -c | \
@@ -11,7 +7,6 @@ grep "^ *<Id>" *.xml | \
 
 # Find duplicated game IDs
 grep "<GameId game=" *.xml | \
-    sed 's/ \(parent\|order\)=\"[^\"]*\"//g' | \
     sed 's/[ \t]*<!--.*-->.*//g' | \
     sort | uniq -c | \
     grep "^ *[2-9]"
@@ -31,15 +26,5 @@ for LANGUAGE_ID in $(grep "<Name " titles.xml | \
                     sort | uniq); do
     if [ -z "$(grep "^ *<Id>${LANGUAGE_ID}</Id>" languages.xml)" ]; then
         echo "The \"${LANGUAGE_ID}\" language does not exit"
-    fi
-done
-
-# Find non-existing location parents (CK2HIP)
-for PARENT_ID in $(grep "game=\"CK2HIP\"" titles.xml | \
-                    grep -e "parent=\"[^\"]\+\"" | \
-                    sed 's/.*parent=\"\([^\"]*\)".*/\1/g' | \
-                    sort | uniq); do
-    if [ -z "$(grep "<GameId game=\"CK2HIP\"" titles.xml | grep ">"${PARENT_ID}"<")" ]; then
-        echo "CK2HIP: ${PARENT_ID} entry does not exit"
     fi
 done
