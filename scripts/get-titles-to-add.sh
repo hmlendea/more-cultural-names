0c4ccc5bcefeb49766283bd2752d7ee35e834a06
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-23 19:45:20 +0300

    Reverted a script

--- scripts/get-titles-to-add.sh
+++ scripts/get-titles-to-add.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+echo "" > titles-to-add.xml
 
-for TITLE_ID in $(grep "CK3" titles.xml | \
-                    sed 's/ *<GameId game=\"CK3\">\([^<]*\)<\/GameId.*/\1/g' | \
+for TITLE_ID in $(grep "^\s*[ekdcb]_" ../ck2-landed-titles-manager/swmh.txt | \
+                    sed 's/\s*=\s*/ = /g' | \
+                    sed 's/^\s*//g' | \
+                    awk '{print $1}' | \
                     sort | uniq); do
-    if [ ! -z "$(grep "${TITLE_ID} = {" ck2mdn.txt)" ]; then
-        echo "${TITLE_ID}"
+    if [ -z "$(grep "<GameId game=\"CK2HIP\"[^>]*>${TITLE_ID}" titles.xml)" ]; then
+        echo "${TITLE_ID} is not defined"
+        echo "  <LocationEntity>" >> titles-to-add.xml
+        echo "    <Id>${TITLE_ID:2}</Id>" >> titles-to-add.xml
+        echo "    <GameIds>" >> titles-to-add.xml
+        echo "      <GameId game=\"CK2HIP\" parent=\"\" order=\"\">${TITLE_ID}</GameId>" >> titles-to-add.xml
+        echo "    </GameIds>" >> titles-to-add.xml
+        echo "    <Names>" >> titles-to-add.xml
+        echo "    </Names>" >> titles-to-add.xml
+        echo "  </LocationEntity>" >> titles-to-add.xml
     fi
 done
