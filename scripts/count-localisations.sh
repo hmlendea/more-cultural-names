4c2f0e57c5a45ebafc669e4199b48acad29caaf4
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-27 20:51:29 +0300

    Imroved the counting script

--- scripts/count-localisations.sh
+++ scripts/count-localisations.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-TITLES_COUNT=$(cat titles.xml | grep "Name language" | wc -l)
-LANGUAGES_COUNT=$(cat languages.xml | grep "/Language>" | wc -l)
+NAMES_COUNT=$(grep "Name language" titles.xml | wc -l)
+LANGUAGES_COUNT=$(grep "/Language>" languages.xml | wc -l)
 
-echo "Titles: ${TITLES_COUNT}"
+function get_game_titles_count() {
+    GAME=${1}
+    TITLES_COUNT=$(grep "<GameId game=\"${GAME}\"" titles.xml | wc -l)
+    echo ${TITLES_COUNT}
+}
+
+function print_game_titles_count() {
+    GAME=${1}
+    TITLES_COUNT=$(get_game_titles_count ${GAME})
+    echo "${GAME} titles: ${TITLES_COUNT}" >&2
+}
+
+print_game_titles_count CK2
+print_game_titles_count CK2HIP
+print_game_titles_count CK3
+print_game_titles_count HOI4
+print_game_titles_count ImperatorRome
+echo ""
+echo "Names: ${NAMES_COUNT}"
 echo "Languages: ${LANGUAGES_COUNT}"
