061a3a6b2a5d9c4d0a642e6c56e15e9ebfe7ed00
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-04-26 23:10:45 +0300

    Extrated the builder update into its own script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 STARTDIR="$(pwd)"
+SCRIPTSDIR="${STARTDIR}/scripts"
 OUTDIR="${STARTDIR}/out"
 EXTRAS_DIR="${STARTDIR}/extras"
 VANILLA_FILES_DIR="${STARTDIR}/vanilla"
@@ -17,31 +18,8 @@ fi
 
 VERSION=$(date +"%y").$(date +"%j").${BUILD_VERSION}
 
-MOD_BUILDER_NAME="more-cultural-names-builder"
-MOD_BUILDER_VERSION=$(curl --silent "https://github.com/hmlendea/${MOD_BUILDER_NAME}/releases/latest" | sed 's/.*\/tag\/v\([^\"]*\)">redir.*/\1/g')
-MOD_BUILDER_ZIP_URL="https://github.com/hmlendea/${MOD_BUILDER_NAME}/releases/download/v${MOD_BUILDER_VERSION}/${MOD_BUILDER_NAME}_${MOD_BUILDER_VERSION}_linux-x64.zip"
-MOD_BUILDER_BIN_FILE_PATH="${STARTDIR}/${MOD_BUILDER_NAME}/MoreCulturalNamesModBuilder"
-NEEDS_DOWNLOADING=true
-
 if [[ $* != *--skip-updates* ]]; then
-    echo "Checking for builder updates..."
-    if [ -d "${STARTDIR}/${MOD_BUILDER_NAME}" ]; then
-        if [ -f "${STARTDIR}/${MOD_BUILDER_NAME}/version.txt" ]; then
-            CURRENT_VERSION=$(cat "${STARTDIR}/${MOD_BUILDER_NAME}/version.txt")
-            if [ "${CURRENT_VERSION}" == "${MOD_BUILDER_VERSION}" ]; then
-                NEEDS_DOWNLOADING=false
-            fi
-        fi
-    fi
-
-    if [ ${NEEDS_DOWNLOADING} == true ]; then
-        [ -d "${STARTDIR}/${MOD_BUILDER_NAME}" ] && rm -rf "${STARTDIR}/${MOD_BUILDER_NAME}"
-
-        wget -c "${MOD_BUILDER_ZIP_URL}"
-        mkdir "${STARTDIR}/${MOD_BUILDER_NAME}"
-        unzip "${STARTDIR}/${MOD_BUILDER_NAME}_${MOD_BUILDER_VERSION}_linux-x64.zip" -d "${STARTDIR}/${MOD_BUILDER_NAME}"
-        echo "${MOD_BUILDER_VERSION}" > "${STARTDIR}/${MOD_BUILDER_NAME}/version.txt"
-    fi
+    bash "${SCRIPTSDIR}/update-builder.sh"
 fi
 
 echo "Validating the files..."
