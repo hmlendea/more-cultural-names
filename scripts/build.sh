3e2dc08f43e464cd2bc9b1e79258c83e3f3f5cdb
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-08-13 10:50:23 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -77,12 +77,12 @@ build-edition \
 
 build-edition \
     "more-cultural-names" "More Cultural Names" \
-    "CK3" "1.3.*" \
+    "CK3" "1.4.*" \
     --landed-titles "vanilla/ck3_landed_titles.txt" --landed-titles-name "999_MoreCulturalNames.txt"
 
 build-edition \
     "ibl-more-cultural-names" "Ibn Battuta's Legacy 2 - More Cultural Names" \
-    "CK3IBL" "1.3.*" \
+    "CK3IBL" "1.4.*" \
     --landed-titles "vanilla/ck3ibl_landed_titles.txt" --landed-titles-name "873_MoreCulturalNames.txt"
 
 build-edition \
