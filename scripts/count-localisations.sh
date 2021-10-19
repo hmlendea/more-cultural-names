bbe85574697cc4582c6b9f7f45f2ba82807101fd
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-19 18:13:54 +0300

    Count well-covered names

--- scripts/count-localisations.sh
+++ scripts/count-localisations.sh
@@ -8,6 +8,8 @@ LOCATIONS_COUNT=$(grep "<Id>" "${LOCATIONS_FILE}" | wc -l)
 LANGUAGES_COUNT=$(grep "/Language>" "${LANGUAGES_FILE}" | wc -l)
 
 WIKIDATA_LOCATIONS_COUNT=$(grep "<WikidataId>" "${LOCATIONS_FILE}" | wc -l)
+WELL_COVERED_LINES_COUNT=$(grep "\(<Name lang\|@@@@ BELOW TITLES NEED REVIEW\)" "${LOCATIONS_FILE}" | grep -n "@@@@ BELOW TITLES NEED REVIEW" | awk -F":" '{print $1}')
+LOCATIONS_LINES_COUNT=$(wc -l "${LOCATIONS_FILE}" | awk '{print $1}')
 
 function get_game_titles_count() {
     GAME=${1}
@@ -35,4 +37,5 @@ echo "Locations: ${LOCATIONS_COUNT}"
 echo "Languages: ${LANGUAGES_COUNT}"
 
 echo ""
-echo "Wikidata coverage: ${WIKIDATA_LOCATIONS_COUNT} ($((WIKIDATA_LOCATIONS_COUNT*100/LOCATIONS_COUNT))%)"
+echo "Well-covered titles: $((WIKIDATA_LOCATIONS_COUNT*100/LOCATIONS_COUNT))% (${WIKIDATA_LOCATIONS_COUNT} titles)"
+echo "Well-covered content: $((WELL_COVERED_LINES_COUNT*100/NAMES_COUNT))% (${WELL_COVERED_LINES_COUNT} names)"
