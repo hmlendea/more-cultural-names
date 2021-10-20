31c558d1727db2a64b9af6682106c06a79ba20cf
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-20 11:23:48 +0300

    Support IR in the unlinked languages finder script

--- scripts/get-unlinked-languages.sh
+++ scripts/get-unlinked-languages.sh
@@ -20,6 +20,10 @@ HOI4_DIR="${STEAM_GAMES_DIR}/Hearts of Iron IV"
 HOI4_TAGS_DIR="${HOI4_DIR}/common/country_tags"
 HOI4_LOCALISATIONS_DIR="${HOI4_DIR}/localisation"
 
+IR_DIR="${STEAM_GAMES_DIR}/ImperatorRome"
+IR_CULTURES_DIR="${IR_DIR}/game/common/cultures"
+IR_LOCALISATIONS_DIR="${IR_DIR}/game/localization"
+
 echo "Crusader Kings 2:"
 for CULTURE_ID in $(cat "${CK2_CULTURES_DIR}/"* | \
                         grep -P '^\t[a-z]* = {' | \
@@ -71,7 +75,54 @@ function getHoi4Countries() {
     done 
 }
 
-echo "Crusader Kings 3:"        && getCk3Cultures   "CK3"       "${CK3_CULTURES_DIR}"
-echo "Crusader Kings 3 IBL:"    && getCk3Cultures   "CK3IBL"    "${CK3IBL_CULTURES_DIR}"
-echo "Crusader Kings 3 MBP:"    && getCk3Cultures   "CK3MBP"    "${CK3MBP_CULTURES_DIR}"
-echo "Hearts of Iron 4:"        && getHoi4Countries "HOI4"      "${HOI4_TAGS_DIR}"
+function getIrCultures() {
+    GAME_ID="${1}" && shift
+    CULTURES_DIR="${@}"
+
+    for CULTURE_FILE_NAME in $(ls "${CULTURES_DIR}/"*.txt); do
+        CULTURE_FILE_JSON=$(cat "${CULTURE_FILE_NAME}" | \
+            sed 's/\r*//g' | \
+            sed '1s/^\xEF\xBB\xBF//' | \
+            sed 's/#.*$//g' | \
+            sed 's/\([a-zA-Z_0-9-]*\)\s*=/\"\1\"=/g' | \
+            sed 's/=\s*\([a-zA-Z0-9][^}]*\)/=\"\1\"/g' | \
+            sed 's/\s*=\s*/:/g' | \
+            sed 's/^}/}\n}/g' | \
+            sed 's/^\"/{\n\"/g' | \
+            sed 's/\"\s*$/\",/g' | \
+            grep -v "hsv\s*{" | \
+            grep -v "^[^{}\:]*$" | \
+            sed 's/}/},/g' | \
+            sed 's/\t/    /g' | \
+            perl -p0e 's/\n/%NL%/g' | \
+            sed 's/{\s*[^\"}]*}/{}/g' | \
+            sed 's/\([}\"]\),\s*%NL%\(\s*\)}/\1%NL%\2}/g' | \
+            sed 's/\([}\"]\),\s*%NL%\(\s*\)}/\1%NL%\2}/g' | \
+            sed 's/}\s*%NL%\(\s*\)\"/},%NL%\1\"/g' | \
+            sed 's/%NL%/ /g' | \
+            sed 's/},\s*$/}/g' | \
+            jq)
+        
+        CULTURE_GROUP_ID=$(echo "${CULTURE_FILE_JSON}" | jq -r 'keys[0]')
+
+        #echo "  ### Culture group: ${CULTURE_GROUP_ID}"
+
+        for CULTURE_ID in $(echo "${CULTURE_FILE_JSON}" | \
+                                jq ".${CULTURE_GROUP_ID}"'.culture' | \
+                                jq 'keys' | \
+                                grep "^\s*\"" | \
+                                sed 's/^\s*\"\([^\"]*\).*/\1/g'); do
+
+            if [ -z "$(grep '<GameId game="'${GAME_ID}'">'${CULTURE_ID}'</GameId>' "${LANGUAGES_FILE}")" ]; then
+                printf "      <GameId game=\"${GAME_ID}\">${CULTURE_ID}</GameId>"
+                printf "\n"
+            fi
+        done
+    done 
+}
+
+echo "Crusader Kings 3:"        && getCk3Cultures   "CK3"           "${CK3_CULTURES_DIR}"
+echo "Crusader Kings 3 IBL:"    && getCk3Cultures   "CK3IBL"        "${CK3IBL_CULTURES_DIR}"
+echo "Crusader Kings 3 MBP:"    && getCk3Cultures   "CK3MBP"        "${CK3MBP_CULTURES_DIR}"
+echo "Hearts of Iron 4:"        && getHoi4Countries "HOI4"          "${HOI4_TAGS_DIR}"
+echo "Imperator Rome:"          && getIrCultures    "ImperatorRome" "${IR_CULTURES_DIR}"
