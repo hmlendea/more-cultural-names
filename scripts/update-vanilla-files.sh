c85ebb0e4f4a138a1c9fd6a521a64f9ad7b4ee3c
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-12-24 14:00:18 +0200

    Script to update the vanilla files

---
+++ scripts/update-vanilla-files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VANILLA_FILES_DIR="$(pwd)/vanilla"
+
+if [ ! -d "${VANILLA_FILES_DIR}" ]; then
+    echo "ERROR: The '${VANILLA_FILES_DIR}' directory does not exist!"
+    exit 1
+fi
+
+STEAM_APPS_DIR="${HOME}/.local/share/Steam/steamapps"
+STEAM_GAMES_DIR="${STEAM_APPS_DIR}/common"
+STEAM_WORKSHOP_DIR="${STEAM_APPS_DIR}/workshop"
+STEAM_WORKSHOP_CK3_DIR="${STEAM_WORKSHOP_DIR}/content/1158310"
+STEAM_WORKSHOP_IR_DIR="${STEAM_WORKSHOP_DIR}/content/859580"
+CK2_LOCAL_MODS_DIR="${HOME}/.paradoxinteractive/Crusader Kings II/mod"
+
+CK2_VANILLA_FILE="${VANILLA_FILES_DIR}/ck2_landed_titles.txt"
+CK2HIP_VANILLA_FILE="${VANILLA_FILES_DIR}/ck2hip_landed_titles.txt"
+CK3_VANILLA_FILE="${VANILLA_FILES_DIR}/ck3_landed_titles.txt"
+CK3IBL_VANILLA_FILE="${VANILLA_FILES_DIR}/ck3ibl_landed_titles.txt"
+CK3MBP_VANILLA_FILE="${VANILLA_FILES_DIR}/ck3mbp_landed_titles.txt"
+CK3TFE_VANILLA_FILE="${VANILLA_FILES_DIR}/ck3tfe_landed_titles.txt"
+
+function update-vanilla-file() {
+    local SOURCE_FILE="${1}"
+    local TARGET_FILE="${2}"
+
+    [ -f "${SOURCE_FILE}" ] && cp "${SOURCE_FILE}" "${TARGET_FILE}"
+}
+
+ls "${CK2_LOCAL_MODS_DIR}/Historical_Immersion_Project/common/landed_titles/swmh_landed_titles.txt"
+
+update-vanilla-file "${STEAM_GAMES_DIR}/Crusader Kings II/common/landed_titles/landed_titles.txt"                       "${CK2_VANILLA_FILE}"
+update-vanilla-file "${CK2_LOCAL_MODS_DIR}/Historical_Immersion_Project/common/landed_titles/swmh_landed_titles.txt"    "${CK2HIP_VANILLA_FILE}"
+update-vanilla-file "${STEAM_GAMES_DIR}/Crusader Kings III/game/common/landed_titles/00_landed_titles.txt"              "${CK3_VANILLA_FILE}"
+update-vanilla-file "${STEAM_WORKSHOP_CK3_DIR}/2416949291/common/landed_titles/00_landed_titles.txt"                    "${CK3IBL_VANILLA_FILE}"
+update-vanilla-file "${STEAM_WORKSHOP_CK3_DIR}/2216670956/common/landed_titles/00_landed_titles.txt"                    "${CK3MBP_VANILLA_FILE}"
+update-vanilla-file "${STEAM_WORKSHOP_CK3_DIR}/2243307127/common/landed_titles/00_landed_titles.txt"                    "${CK3TFE_VANILLA_FILE}"
