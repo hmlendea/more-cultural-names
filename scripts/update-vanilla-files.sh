a6a31519592271e0b669dbb8edabd56e02be38ae
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-01-23 22:01:52 +0200

    Support for all Apotheosis landed titles

--- scripts/update-vanilla-files.sh
+++ scripts/update-vanilla-files.sh
@@ -29,8 +29,25 @@ function update-vanilla-file() {
     local TARGET_FILE="${2}"
     local SOURCE_URL="${3}"
 
-    if [ -f "${SOURCE_FILE}" ]; then
-        cp "${SOURCE_FILE}" "${TARGET_FILE}"
+    if compgen -G "${SOURCE_FILE}" > /dev/null; then
+        cat "${SOURCE_FILE}" > "${TARGET_FILE}"
+    elif [ -n "${SOURCE_URL}" ]; then
+        wget -qc --no-check-certificate "${SOURCE_URL}" -O "${TARGET_FILE}" 2>/dev/null
+    fi
+
+    if [ -f "${TARGET_FILE}" ]; then
+        chmod 755 "${TARGET_FILE}"
+        chown "${USER}:${USER}" "${TARGET_FILE}"
+    fi
+}
+
+function update-vanilla-files() {
+    local SOURCE_DIR="${1}"
+    local TARGET_FILE="${2}"
+    local SOURCE_URL="${3}"
+
+    if [ -d "${SOURCE_DIR}" ]; then
+        cat "${SOURCE_DIR}"/*.txt > "${TARGET_FILE}"
     elif [ -n "${SOURCE_URL}" ]; then
         wget -qc --no-check-certificate "${SOURCE_URL}" -O "${TARGET_FILE}" 2>/dev/null
     fi
@@ -50,6 +67,9 @@ update-vanilla-file \
 update-vanilla-file \
     "${STEAM_GAMES_DIR}/Crusader Kings III/game/common/landed_titles/00_landed_titles.txt" \
     "${CK3_VANILLA_FILE}"
+update-vanilla-files \
+    "${STEAM_WORKSHOP_CK3_DIR}/2618149514/common/landed_titles" \
+    "${CK3ATHA_VANILLA_FILE}"
 update-vanilla-file \
     "${STEAM_WORKSHOP_CK3_DIR}/2416949291/common/landed_titles/00_landed_titles.txt" \
     "${CK3IBL_VANILLA_FILE}"
@@ -63,9 +83,6 @@ update-vanilla-file \
 update-vanilla-file \
     "${STEAM_WORKSHOP_CK3_DIR}/2243307127/common/landed_titles/00_landed_titles.txt" \
     "${CK3TFE_VANILLA_FILE}"
-update-vanilla-file \
-    "${STEAM_WORKSHOP_CK3_DIR}/2618149514/common/landed_titles/00_landed_titles.txt" \
-    "${CK3ATHA_VANILLA_FILE}"
 update-vanilla-file \
     "${STEAM_GAMES_DIR}/ImperatorRome/game/localization/english/provincenames_l_english.yml" \
     "${IR_VANILLA_FILE}"
