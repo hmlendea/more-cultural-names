65c94b978d16e2dd73f9765fde855050cfe9bb40
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-02-20 00:26:41 +0200

    Linked most CK2TWK languages

--- scripts/get-unlinked-languages.sh
+++ scripts/get-unlinked-languages.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 source "scripts/common/paths.sh"
 
-echo "Crusader Kings 2:"
+function getCk2Cultures() {
+    local GAME_ID="${1}" && shift
+    local CULTURES_DIR="${*}"
 
-for CULTURE_ID in $(grep -P '^[\t ][a-z_]* = {' "${CK2_CULTURES_DIR}/"* | \
-                    grep -v "\(alternate_start\|graphical_cultures\|mercenary_names\)" | \
-                    awk -F":" '{print $2}' | \
-                    sed 's/^[\t ]*//g' | \
-                    awk -F" " '{print $1}' | \
-                    sort | uniq); do
-    if ! grep -q '<GameId game="CK2">'${CULTURE_ID}'</GameId>' "${LANGUAGES_FILE}"; then
-        echo "  ${CULTURE_ID}"
-    fi
-done
-
-echo "Crusader Kings 2 HIP:"
-for CULTURE_ID in $(grep -P '^\s[a-z_]* = {' "${CK2HIP_CULTURES_DIR}/"* | \
-                    grep -v "\(alternate_start\|graphical_cultures\|hip_culture\|mercenary_names\)" | \
-                    awk -F":" '{print $2}' | \
-                    sed 's/^\s*//g' | \
-                    awk -F" " '{print $1}' | \
-                    sort | uniq); do
-    if ! grep -q '<GameId game="CK2HIP">'${CULTURE_ID}'</GameId>' "${LANGUAGES_FILE}"; then
-        echo "  ${CULTURE_ID}"
-    fi
-done
+    for CULTURE_ID in $(grep -P '^\s[a-z_]* = {' "${CULTURES_DIR}/"* | \
+                        grep -v "\(alternate_start\|graphical_cultures\|hip_culture\|mercenary_names\)" | \
+                        awk -F":" '{print $2}' | \
+                        sed 's/^\s*//g' | \
+                        awk -F" " '{print $1}' | \
+                        sort | uniq); do
+        if ! grep -q "<GameId game=\"${GAME_ID}\">${CULTURE_ID}</GameId>" "${LANGUAGES_FILE}"; then
+            echo "  ${CULTURE_ID}"
+        fi
+    done
+}
 
 function getCk3v14Cultures() {
     local GAME_ID="${1}" && shift
@@ -123,12 +114,15 @@ function getIrCultures() {
     done 
 }
 
-echo "Crusader Kings 3:"        && getCk3Cultures       "CK3"       "${CK3_CULTURES_DIR}"
-echo "Crusader Kings 3 ATHA:"   && getCk3Cultures       "CK3ATHA"   "${CK3ATHA_CULTURES_DIR}"
-echo "Crusader Kings 3 IBL:"    && getCk3Cultures       "CK3IBL"    "${CK3IBL_CULTURES_DIR}"
-echo "Crusader Kings 3 MBP:"    && getCk3Cultures       "CK3MBP"    "${CK3MBP_CULTURES_DIR}"
-echo "Crusader Kings 3 TFE:"    && getCk3v14Cultures    "CK3TFE"    "${CK3TFE_CULTURES_DIR}"
-echo "Hearts of Iron 4:"        && getHoi4Countries     "HOI4"      "${HOI4_TAGS_DIR}" "${HOI4_LOCALISATIONS_DIR}"
-echo "Hearts of Iron 4 TGW:"    && getHoi4Countries     "HOI4TGW"   "${HOI4TGW_TAGS_DIR}" "${HOI4TGW_LOCALISATIONS_DIR}"
-echo "Imperator Rome:"          && getIrCultures        "IR"        "${IR_CULTURES_DIR}"
-echo "Imperator Rome:"          && getIrCultures        "IR_AoE"    "${IR_AoE_CULTURES_DIR}"
+echo "Crusader Kings 2:"        && getCk2Cultures       "CK2"       "${CK2_CULTURES_DIR}"
+echo "Crusader Kings 2 HIP:"    && getCk2Cultures       "CK2HIP"    "${CK2HIP_CULTURES_DIR}"
+echo "Crusader Kings 2 TWK:"    && getCk2Cultures       "CK2TWK"    "${CK2TWK_CULTURES_DIR}"
+#echo "Crusader Kings 3:"        && getCk3Cultures       "CK3"       "${CK3_CULTURES_DIR}"
+#echo "Crusader Kings 3 ATHA:"   && getCk3Cultures       "CK3ATHA"   "${CK3ATHA_CULTURES_DIR}"
+#echo "Crusader Kings 3 IBL:"    && getCk3Cultures       "CK3IBL"    "${CK3IBL_CULTURES_DIR}"
+#echo "Crusader Kings 3 MBP:"    && getCk3Cultures       "CK3MBP"    "${CK3MBP_CULTURES_DIR}"
+#echo "Crusader Kings 3 TFE:"    && getCk3v14Cultures    "CK3TFE"    "${CK3TFE_CULTURES_DIR}"
+#echo "Hearts of Iron 4:"        && getHoi4Countries     "HOI4"      "${HOI4_TAGS_DIR}" "${HOI4_LOCALISATIONS_DIR}"
+#echo "Hearts of Iron 4 TGW:"    && getHoi4Countries     "HOI4TGW"   "${HOI4TGW_TAGS_DIR}" "${HOI4TGW_LOCALISATIONS_DIR}"
+#echo "Imperator Rome:"          && getIrCultures        "IR"        "${IR_CULTURES_DIR}"
+#echo "Imperator Rome:"          && getIrCultures        "IR_AoE"    "${IR_AoE_CULTURES_DIR}"
