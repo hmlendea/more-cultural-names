8ea04d1ea419d840a95782a9b4ab94b0a21ffd6d
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-02-22 00:23:43 +0200

    Improved the vanilla file updating script

--- scripts/update-vanilla-files.sh
+++ scripts/update-vanilla-files.sh
@@ -15,6 +15,7 @@ function update-vanilla-file() {
     if [ -f "${TARGET_FILE}" ]; then
         chmod 755 "${TARGET_FILE}"
         chown "${USER}:${USER}" "${TARGET_FILE}"
+        sed -i 's/\r$//' "${TARGET_FILE}"
     fi
 }
 
@@ -32,35 +33,36 @@ function update-vanilla-files() {
     if [ -f "${TARGET_FILE}" ]; then
         chmod 755 "${TARGET_FILE}"
         chown "${USER}:${USER}" "${TARGET_FILE}"
+        sed -i 's/\r$//' "${TARGET_FILE}"
     fi
 }
 
 update-vanilla-file \
-    "${STEAM_GAMES_DIR}/Crusader Kings II/common/landed_titles/landed_titles.txt" \
+    "${CK2_DIR}/common/landed_titles/landed_titles.txt" \
     "${CK2_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-file \
-    "${CK2_LOCAL_MODS_DIR}/Historical_Immersion_Project/common/landed_titles/swmh_landed_titles.txt"\
+    "${CK2HIP_DIR}/common/landed_titles/swmh_landed_titles.txt"\
     "${CK2HIP_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-file \
-    "${STEAM_GAMES_DIR}/Crusader Kings III/game/common/landed_titles/00_landed_titles.txt" \
+    "${CK3_DIR}/game/common/landed_titles/00_landed_titles.txt" \
     "${CK3_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-files \
-    "${STEAM_WORKSHOP_CK3_DIR}/2618149514/common/landed_titles" \
+    "${CK3ATHA_DIR}/common/landed_titles" \
     "${CK3ATHA_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-file \
-    "${STEAM_WORKSHOP_CK3_DIR}/2416949291/common/landed_titles/00_landed_titles.txt" \
+    "${CK3IBL_DIR}/common/landed_titles/00_landed_titles.txt" \
     "${CK3IBL_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-file \
-    "${STEAM_WORKSHOP_CK3_DIR}/2216670956/common/landed_titles/00_landed_titles.txt" \
+    "${CK3MBP_DIR}/common/landed_titles/00_landed_titles.txt" \
     "${CK3MBP_VANILLA_LANDED_TITLES_FILE}" \
     "https://raw.githubusercontent.com/Leviathonlx/MoreBookmarks-Plus/main/common/landed_titles/00_landed_titles.txt"
 update-vanilla-file \
-    "${STEAM_WORKSHOP_CK3_DIR}/2243307127/common/landed_titles/00_landed_titles.txt" \
+    "${CK3TFE_DIR}/common/landed_titles/00_landed_titles.txt" \
     "${CK3TFE_VANILLA_LANDED_TITLES_FILE}"
 update-vanilla-file \
-    "${STEAM_GAMES_DIR}/ImperatorRome/game/localization/english/provincenames_l_english.yml" \
+    "${IR_LOCALISATION_DIR}/provincenames_l_english.yml" \
     "${IR_VANILLA_FILE}"
 update-vanilla-file \
-    "${STEAM_WORKSHOP_IR_DIR}/2578689167/localization/english/replace/aoe_provincenames_l_english.yml" \
+    "${IR_AoE_LOCALISATION_DIR}/replace/aoe_provincenames_l_english.yml" \
     "${IR_AoE_VANILLA_FILE}" \
     "https://raw.githubusercontent.com/EliteNinjaX/dark_age_mod/Stable/localization/english/replace/aoe_provincenames_l_english.yml"
