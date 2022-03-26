3d6c4cc37c46fcc38ce2b978c2719f5bcf922e8f
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-03-26 17:06:57 +0200

    Added the build target

--- scripts/build.sh
+++ scripts/build.sh
@@ -116,6 +116,11 @@ build-edition \
     --landed-titles "${VANILLA_FILES_DIR}/ck3mbp_landed_titles.txt" --landed-titles-name "873_MoreCulturalNames.txt"
 
 build-edition \
+    "sow-more-cultural-names" "Sinews of War - More Cultural Names" \
+    "CK3SoW" "1.5.*" \
+    --landed-titles "${CK3SoW_VANILLA_LANDED_TITLES_FILE}" --landed-titles-name "873_MoreCulturalNames.txt"
+
+build-edition \
     "tfe-more-cultural-names" "The Fallen Eagle: More Cultural Names" \
     "CK3TFE" "1.5.*" \
     --landed-titles "${VANILLA_FILES_DIR}/ck3tfe_landed_titles.txt" --landed-titles-name "873_MoreCulturalNames.txt"
