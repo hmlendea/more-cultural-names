4c1e9549bd5b9f8095ab145ef3d3233631e800e4
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-06-17 12:30:01 +0300

    Fixed build version

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,14 @@ BUILDER_VERSION=""
 
 [ -f "${REPO_DIR}/.builder/version.txt" ] && BUILDER_VERSION=$(cat "${REPO_DIR}/.builder/version.txt")
 
+if [ -z "${BUILD_VERSION}" ] || ! [[ ${BUILD_VERSION} =~ ^[0-9]+$ ]]; then
+    BUILD_VERSION=0
+fi
+
 VERSION=$(date +"%y").$(date +"%j").${BUILD_VERSION}
 
 DATA_CONTENT=$(cat "${REPO_DIR}"/*.xml)
 CHECKSUM=$(echo "${CHECKSUM_SEED} ${VERSION} ${BUILDER_VERSION}" | sha512sum | awk '{print $1}')
-
-if [ -z "${BUILD_VERSION}" ] || ! [[ ${BUILD_VERSION} =~ ^[0-9]+$ ]]; then
-    BUILD_VERSION=0
-fi
 
 if [[ $* != *--skip-updates* ]]; then
     bash "${SCRIPTS_DIR}/update-builder.sh"
