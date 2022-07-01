7764416f7e18b163467f3bd1dec2a2a743f12860
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-07-01 16:36:40 +0300

    Target the latest CK3IBL

--- scripts/build.sh
+++ scripts/build.sh
@@ -114,7 +114,7 @@ build-edition \
 
 build-edition \
     "ibl-more-cultural-names" "Ibn Battuta's Legacy 2 - More Cultural Names" \
-    "CK3IBL" "1.5.*" \
+    "CK3IBL" "1.6.*" \
     --landed-titles "${VANILLA_FILES_DIR}/ck3ibl_landed_titles.txt" --landed-titles-name "873_MoreCulturalNames.txt"
 
 build-edition \
