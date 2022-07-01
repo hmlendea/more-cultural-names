8ae6191c0a2bce576b4f161e4d4107462b81c304
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-07-01 22:19:57 +0300

    Improved the redundant language finder

--- scripts/find-redundant-languages.sh
+++ scripts/find-redundant-languages.sh
@@ -2,9 +2,9 @@
 source "scripts/common/paths.sh"
 
 LANGUAGE_IDS="$(grep "<Id>" "${LANGUAGES_FILE}" | sed 's/[^>]*>\([^<]*\).*/\1/g' | sort)"
-LOCATIONS_FILE_CONTENT=$(cat "${LOCATIONS_FILE}")
+LOCATIONS_FILE_LANGUAGE_IDS=$(grep "<Name language=" "${LOCATIONS_FILE}" | sed 's/^.*language=\"\([^\"]*\).*/\1/g' | sort | uniq)
 
 # The results are just *potentially* redundant languages
 for LANGUAGE_ID in ${LANGUAGE_IDS}; do
-    ! grep -q "${LANGUAGE_ID}" <<< "${LOCATIONS_FILE_CONTENT}" && echo "${LANGUAGE_ID}"
+    ! grep -q "${LANGUAGE_ID}" <<< "${LOCATIONS_FILE_LANGUAGE_IDS}" && echo "${LANGUAGE_ID}"
 done
