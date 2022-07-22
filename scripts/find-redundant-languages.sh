13328cda11219d95e83863ded056ced64d0afb7d
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-07-22 15:19:05 +0300

    Improved the redundant language finder

--- scripts/find-redundant-languages.sh
+++ scripts/find-redundant-languages.sh
@@ -3,8 +3,17 @@ source "scripts/common/paths.sh"
 
 LANGUAGE_IDS="$(grep "<Id>" "${LANGUAGES_FILE}" | sed 's/[^>]*>\([^<]*\).*/\1/g' | sort)"
 LOCATIONS_FILE_LANGUAGE_IDS=$(grep "<Name language=" "${LOCATIONS_FILE}" | sed 's/^.*language=\"\([^\"]*\).*/\1/g' | sort | uniq)
+LANGUAGES_FILE_CONTENT=$(cat "${LANGUAGES_FILE}")
 
 # The results are just *potentially* redundant languages
 for LANGUAGE_ID in ${LANGUAGE_IDS}; do
-    ! grep -q "${LANGUAGE_ID}" <<< "${LOCATIONS_FILE_LANGUAGE_IDS}" && echo "${LANGUAGE_ID}"
+    LANGUAGE_IS_REDUNDANT=true
+
+    if grep -q "${LANGUAGE_ID}" <<< "${LOCATIONS_FILE_LANGUAGE_IDS}" \
+    || grep -q "<LanguageId>${LANGUAGE_ID}</LanguageId>" <<< "${LANGUAGES_FILE_CONTENT}" \
+    || grep -Pzoq "\n\s*<Id>${LANGUAGE_ID}</Id>.*(\n\s*<Code.*)*\n\s*<(GameId|Fallback)" <<< "${LANGUAGES_FILE_CONTENT}"; then
+        LANGUAGE_IS_REDUNDANT=false
+    fi
+
+    ${LANGUAGE_IS_REDUNDANT} && echo "${LANGUAGE_ID}"
 done
