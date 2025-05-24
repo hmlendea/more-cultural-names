7da35be0d262271b00c132c52bfe583f7ade416f
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-05-24 20:03:51 +0300

    ck3mbp: Updated the build target

--- scripts/build.sh
+++ scripts/build.sh
@@ -134,7 +134,7 @@ build-edition \
 
 build-edition \
     'mbp-more-cultural-names' 'More Bookmarks+ - More Cultural Names' \
-    'CK3MBP' '1.14.*' \
+    'CK3MBP' '1.16.*' \
     --landed-titles "${VANILLA_FILES_DIR}/ck3mbp_landed_titles.txt" --landed-titles-name '873_MoreCulturalNames.txt'
 
 build-edition \
